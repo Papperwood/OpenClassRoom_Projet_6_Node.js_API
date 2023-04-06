5e464da4b0a7f623c3d41cb21868ddc4d8f54347
Papperwood <mauryalexandre.fgmail.com>
2023-04-06 17:40:56 +0200

    refactor : commentary  models

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,17 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // importe le module mongoose pour utilisation ici meme
 
+// définit un nouveau schéma Mongoose appelé sauceSchema. schema a respect pour les post et autres
 const sauceSchema = mongoose.Schema({
-  userId: { type: String, required: true },
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
-  likes: { type: Number, default: 0 },
-	dislikes: { type: Number, default: 0 },
-	usersLiked: { type: [String] },
-	usersDisliked: { type: [String] },
+  userId: { type: String, required: true }, // L'identifiant doit être une chaîne de caractères + obligatoire pour chaque sauce.
+  name: { type: String, required: true }, // idem
+  manufacturer: { type: String, required: true }, // idem
+  description: { type: String, required: true }, // idem
+  mainPepper: { type: String, required: true }, // idem
+  imageUrl: { type: String, required: true }, // idem
+  heat: { type: Number, required: true }, // Le niveau de piquant doit être un nombre + Obligatoire
+  likes: { type: Number, default: 0 }, // Le nombre de "j'aime" doit être un nombre + par default 0
+	dislikes: { type: Number, default: 0 }, // idem likes
+	usersLiked: { type: [String] }, // doit etre une chaine de caractere + permet de garder une trace des utilisateurs qui ont aimé la sauce et d'empêcher un utilisateur d'aimer la sauce plusieurs fois.
+	usersDisliked: { type: [String] }, // idem usersLiked
 });
 
-module.exports = mongoose.model("Sauce", sauceSchema);
+module.exports = mongoose.model("Sauce", sauceSchema); // exporte un modèle Mongoose basé sur le schéma sauceSchema défini précédemment.
--- models/User.js
+++ models/User.js
@@ -1,11 +1,12 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require("mongoose"); // importe le module mongoose pour utilisation ici meme
+const uniqueValidator = require("mongoose-unique-validator"); // importe le module unique-validator
 
-const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+// définit un nouveau schéma Mongoose appelé userSchema ( c'est une modele a respecter pour sign up puis sign in ...)
+const userSchema = mongoose.Schema({ 
+  email: { type: String, required: true, unique: true }, // L'adresse e-mail doit être une chaîne de caractères + obligatoire + unique dans la base de données.
+  password: { type: String, required: true }, // Le mot de passe doit être une chaîne de caractères + obligatoire
 });
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator); // ajoute le plugin uniqueValidator au schéma userSchema
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema); // exporte un modèle Mongoose basé sur le schéma userSchema défini précédemment.
